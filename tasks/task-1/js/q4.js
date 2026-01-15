8eb506c113734ce6ceadf3731491441a148cae0a
Lucie! <lsoussana@hotmail.com>
2026-01-15 17:24:32 -0500

    I am going to crash out. Q5 is not working.

--- tasks/task-1/js/q4.js
+++ tasks/task-1/js/q4.js
@@ -4,24 +4,74 @@ function setup() {
     createCanvas(600, 600);
 }
 
+let rect1 = {
+    r: 255,
+    g: 244,
+    b: 233
+}
+
+let rect2 = {
+    r: 255,
+    g: 182,
+    b: 203
+}
+
+let rect3 = {
+    r: 126,
+    g: 63,
+    b: 18
+}
+
 function draw() {
-    background(0, 0, 0);
-
     push();
     noStroke();
-    fill(200, 180, 100);
-    rect(400, 0, 600);
+    fill(rect1.r, rect1.g, rect1.b);
+    rect(0, 0, 200, 600);
     pop();
 
     push();
     noStroke();
-    fill(200, 50, 150);
-    ellipse(50, 50, 40);
+    fill(rect2.r, rect2.g, rect2.b);
+    rect(200, 0, 200, 600);
     pop();
 
     push();
     noStroke();
-    fill(125, 200, 50);
-    ellipse(100, 100, 80);
+    fill(rect3.r, rect3.g, rect3.b);
+    rect(400, 0, 200, 600);
     pop();
+
+    mouseHover();
+}
+
+function mouseHover() {
+    if (mouseX < 200) {
+        rect1.r = 78;
+        rect1.g = 159;
+        rect1.b = 229;
+    } else {
+        rect1.r = 255;
+        rect1.g = 244;
+        rect1.b = 233;
+    }
+
+    if (mouseX >= 200 && mouseX < 400) {
+        rect2.r = 78;
+        rect2.g = 159;
+        rect2.b = 229;
+    } else {
+        rect2.r = 255;
+        rect2.g = 182;
+        rect2.b = 203;
+    }
+
+    if (mouseX >= 400) {
+        rect3.r = 78;
+        rect3.g = 159;
+        rect3.b = 229;
+    } else {
+        rect3.r = 126;
+        rect3.g = 63;
+        rect3.b = 18;
+    }
 }
