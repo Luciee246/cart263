493db3e107851fdf869457bd4ca463ae8ee114cc
Lucie! <lsoussana@hotmail.com>
2026-01-18 19:16:38 -0500

    Actually finished q7 now

--- tasks/task-1/js/q7.js
+++ tasks/task-1/js/q7.js
@@ -1,5 +1,9 @@
 "use strict";
 
+let red;
+let green;
+let blue;
+
 function setup() {
     createCanvas(600, 600);
     red = random(255);
@@ -8,22 +12,11 @@ function setup() {
 }
 
 let radius = 30;
-let red;
-let green;
-let blue;
+
 let circle = true;
 
 function draw() {
     background(0);
-
-    for (let i = 0; i < 20; i++) {
-        for (let j = 0; j < 20; j++) {
-            push();
-            fill(red, green, blue);
-            ellipse(radius * i + radius / 2, j * radius + radius / 2, radius);
-            pop();
-        }
-    }
 
     for (let i = 0; i < 20; i++) {
         for (let j = 0; j < 20; j++) {
@@ -38,20 +31,21 @@ function draw() {
         }
     }
 
-    function keyPressed() {
-        if (keyCode === 32) {
-            red = random(255);
-            green = random(255);
-            blue = random(255);
-        }
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+        red = random(255);
+        green = random(255);
+        blue = random(255);
     }
+}
 
-    function mousePressed() {
-        if (circle == true) {
-            circle = false;
+function mousePressed() {
+    if (circle == true) {
+        circle = false;
 
-        } else {
-            circle = true;
-        }
+    } else {
+        circle = true;
     }
 }
