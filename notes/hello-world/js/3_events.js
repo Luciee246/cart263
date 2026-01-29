85d1da3bfdf63d73d51ebbb4b4a29f54de114c3a
Lucie! <lsoussana@hotmail.com>
2026-01-29 15:14:12 -0500

    Finished HTML & JS lesson

--- notes/hello-world/js/3_events.js
+++ notes/hello-world/js/3_events.js
@@ -1,13 +1,40 @@
 window.onload = setup;
 function setup() {
-    console.log("events!")
+    let selection = document.querySelectorAll(".mouseclick-active-section");
 
-    let introSection = document.querySelector("#intro");
-    introSection.addEventListener("click", function (e) {
-        console.log(this);
-        console.log(e)
+    selection.forEach((element) => {
+        element.addEventListener("click", function (e) {
 
-        //a:
-        this.style.background = `rgba(214, 110, 239, 0.5)`
+            if (this.getAttribute("custom-bool") == "inactive") {
+
+                this.setAttribute("custom-bool", "active");
+
+                console.log("is inactive");
+                console.log(this.id);
+
+                this.style.opacity = "0.5";
+            }
+            else {
+                this.setAttribute("custom-bool", "inactive");
+
+                this.style.opacity = "1.0";
+            }
+
+            document.querySelector("#bubbleButton").addEventListener("click", function () {
+                console.log("button clicked");
+
+                let bubble = document.createElement("div");
+                bubble.classList.add("bubble");
+                bubble.style.left = `${Math.random() * (window.innerWidth - 200)}px`;
+                bubble.style.top = `${Math.random() * (window.innerHeight - 200)}px`;
+
+                let r = Math.ceil(Math.random() * 255);
+                let g = Math.ceil(Math.random() * 255);
+                let b = Math.ceil(Math.random() * 255);
+
+                bubble.style.background = `rgba(${r},${g},${b})`;
+                document.getElementById("top-layer").appendChild(bubble)
+            });
+        });
     });
 }
