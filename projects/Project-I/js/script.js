aa18a40b5d809aa125114099b0e7df7bd5177c7d
Lucie! <lsoussana@hotmail.com>
2026-02-04 17:40:40 -0500

    Working on base idea game
    Still need to finish bird theme, then normal, then music
    
    Also need to add a scroll bar for difficulty, and timer needs to go down

--- projects/Project-I/js/script.js
+++ projects/Project-I/js/script.js
@@ -14,3 +14,30 @@
  */
 
 "use strict";
+
+window.onload = setup;
+
+function setup() {
+    let birds = document.querySelector('.birds').textContent;
+
+    let textInput = document.querySelector('#textInput');
+    textInput.addEventListener("keydown", function (e) {
+        if (e.which === 13) {
+            const prompt = document.querySelector('.prompt').textContent;
+            // const answer = document.querySelector()
+            // if (answer.includes(prompt) && birds.includes(textInput.value)) {
+            //     console.log("correct");
+            // }
+
+            console.log(textInput.value.toUpperCase());
+            // const birds = birds.textContent.toLowerCase();
+            const answer = textInput.value.toLowerCase();
+            const result = birds.includes(answer + "\n");
+
+
+            if (result == true && birdsUp.includes(answer + "\n")) {
+                console.log("correct");
+            }
+        }
+    })
+}
