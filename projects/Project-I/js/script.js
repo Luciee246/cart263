adef22d12084f38292253a7e0189f52b6086b312
Lucie! <lsoussana@hotmail.com>
2026-02-04 15:50:13 -0500

    Started project I coding

---
+++ projects/Project-I/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Word game
+ * Lucie Soussana, Jake Hayduk
+ * 
+ * A game of making words with the bigrams given
+ * 
+ * Instructions:
+ * - Bigrams will appear on the screen
+ * - Make words using the bigrams
+ * - Get a high score before time runs out!
+ * 
+ * Made with p5
+ * https://p5js.org/
+ */
+
+"use strict";
