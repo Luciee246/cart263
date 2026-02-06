eeae209dc8968f86fc2ce4df2b6a02b63fe0a1cb
Lucie! <lsoussana@hotmail.com>
2026-02-05 19:49:59 -0500

    Fixed up some bugs on the project I, almost finished the base game

--- projects/Project-I/js/script.js
+++ projects/Project-I/js/script.js
@@ -23,20 +23,28 @@ function setup() {
     let textInput = document.querySelector('#textInput');
     textInput.addEventListener("keydown", function (e) {
         if (e.which === 13) {
-            const prompt = document.querySelector('.prompt').textContent;
+            const prompt = document.querySelector('.prompt').textContent.toLowerCase();
             // const answer = document.querySelector()
             // if (answer.includes(prompt) && birds.includes(textInput.value)) {
             //     console.log("correct");
             // }
 
-            console.log(textInput.value.toUpperCase());
             // const birds = birds.textContent.toLowerCase();
             const answer = textInput.value.toLowerCase();
-            const result = birds.includes(answer + "\n");
+            const result = birds.includes("\n" + answer + "\n");
+            const checkInclude = answer.includes(prompt);
 
 
-            if (result == true && birdsUp.includes(answer + "\n")) {
+            if (result == true && checkInclude == true) {
                 console.log("correct");
+                textInput.value = "";
+            }
+
+            else {
+                textInput.style.color = "red";
+                setTimeout(function () {
+                    textInput.style.color = "black";
+                }, 500)
             }
         }
     })
