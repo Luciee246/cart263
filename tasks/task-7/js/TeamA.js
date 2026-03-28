64dc9df217ac3f8f2b3a3c247c5be6783b2a8061
Lucie! <lsoussana@hotmail.com>
2026-03-28 10:42:05 -0400

    Added step 2!

--- tasks/task-7/js/TeamA.js
+++ tasks/task-7/js/TeamA.js
@@ -49,9 +49,65 @@ export class PlanetA {
         this.planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
         this.group.add(this.planetMesh);
 
-        //STEP 2: 
-        //TODO: Add from 1 to 3 orbiting moons to the planet group. 
+        //STEP 2:
+        //TODO: Add from 1 to 3 orbiting moons to the planet group.
         //TODO: The moons should rotate around the planet just like the planet group rotates around the Sun.
+
+        / Moons */
+        this.moons = [];
+
+        // Create 3 moons
+        const moonCount = 3;
+        for (let i = 0; i < moonCount; i++) {
+            //Pivot group for moon orbit
+            const moonPivot = new THREE.Group();
+
+            / Moon mesh /
+            const moonGeometry = new THREE.SphereGeometry(0.4, 32, 32);
+            const moonMaterial = new THREE.MeshStandardMaterial({
+                color: 0x00ccff,
+                emissive: 0x003333,
+                emissiveIntensity: 0.6,
+                roughness: 0.4,
+                metalness: 0.2
+            });
+
+            const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+
+            //Make them glow
+            const glowGeometry = new THREE.SphereGeometry(0.55, 32, 32);
+            const glowMaterial = new THREE.MeshBasicMaterial({
+                color: 0x00ffaa,
+                transparent: true,
+                opacity: 0.3,
+            });
+
+            const glow = new THREE.Mesh(glowGeometry, glowMaterial);
+            moon.add(glow);
+
+            //Add light
+            const moonLight = new THREE.PointLight(0x00ffaa, 0.5, 10);
+            moon.add(moonLight);
+
+            //Shadows
+            moon.castShadow = true;
+            moon.receiveShadow = true;
+
+            //Orbit radius moon and planet
+            moon.position.x = 3 + i * 0.5;
+
+            //Add moon to pivot group
+            moonPivot.add(moon);
+
+            //Add pivot group to planet group
+            this.group.add(moonPivot);
+
+            //Save for animation
+            this.moons.push({
+                pivot: moonPivot,
+                speed: 0.5 + Math.random()
+            });
+        }
 
         //STEP 3:
         //TODO: Load Blender models to populate the planet with multiple props and critters by adding them to the planet group.
@@ -88,6 +144,12 @@ export class PlanetA {
         }
 
         //TODO: Do the moon orbits and the model animations here.
+        // Rotate planet
+        this.group.rotation.y += delta * 0.5;
+        // Rotate moons
+        this.moons.forEach(moon => {
+            moon.pivot.rotation.y += delta * moon.speed;
+        });
     }
 
     click(mouse, scene, camera) {
